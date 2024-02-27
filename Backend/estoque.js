5aae037122a830b54df1e9e805befd9432b2ba0d
Gardiny <109247332+IGardiny@users.noreply.github.com>
2024-02-26 22:27:45 -0300

    estoqueb

--- Backend/estoque.js
+++ Backend/estoque.js
@@ -1,7 +1,11 @@
 function buscar() { // mini infor para teste
     const exemploDados = [
-        { id: 1, dataSolicitada: '2024-02-26', status: 'concluido', setor: 'Armazém', tipo: 'Produto A', dataEntrega: '2024-03-05', prioridade: 'alta' },
-        { id: 2, dataSolicitada: '2024-02-25', status: 'pendente', setor: 'Logística', tipo: 'Produto B', dataEntrega: '2024-03-10', prioridade: 'baixa' }
+        { id: 1, dataSolicitada: '2024-02-26', status: 'concluido', setor: 'Ti', tipo: 'Produto A', dataEntrega: '2024-03-05', prioridade: 'Alta' },
+        { id: 2, dataSolicitada: '2024-02-25', status: 'pendente', setor: 'Ufac', tipo: 'Produto B', dataEntrega: '2024-03-10', prioridade: 'Media' },
+        { id: 3, dataSolicitada: '2024-02-25', status: 'pendente', setor: 'Ifac', tipo: 'Produto C', dataEntrega: '2024-03-10', prioridade: 'Alta' },
+        { id: 4, dataSolicitada: '2024-02-25', status: 'pendente', setor: 'Uninorte', tipo: 'Produto H', dataEntrega: '2024-03-10', prioridade: 'Media' },
+        { id: 5, dataSolicitada: '2024-02-25', status: 'pendente', setor: 'Unimeta', tipo: 'Produto Y', dataEntrega: '2024-05-10', prioridade: 'Baixa' },
+        { id: 6, dataSolicitada: '2024-02-25', status: 'pendente', setor: 'naosei', tipo: 'Produto Z', dataEntrega: '2024-02-10', prioridade: 'Baixa' }
 
     ];
 
