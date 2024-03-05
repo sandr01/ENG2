0563b85a1a08480db084021a92eec52e277df239
Gardiny <igor20091983@gmail.com>
2024-03-05 13:09:55 -0300

    loginn

--- server.js
+++ server.js
@@ -3,14 +3,27 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-    const filePath = path.join(__dirname, '/index.html', req.url);
+    let filePath = path.join(__dirname, 'Frontend', req.url);
+
+    // Se o caminho leva a um diretório, adicione 'index.html' ao final
+    if (fs.statSync(filePath).isDirectory()) {
+        filePath = path.join(filePath, './Frontend/index.html');
+    }
+
+    // Obtenha a extensão do arquivo
     const extname = path.extname(filePath);
+
+    // Mapeie extensões de arquivo para tipos de conteúdo MIME
     const contentTypeMap = {
         '.html': 'text/html',
         '.css': 'text/css',
         '.js': 'text/javascript',
+        // Adicione mais tipos conforme necessário
     };
+
+    // Determine o tipo de conteúdo com base na extensão do arquivo
     const contentType = contentTypeMap[extname] || 'application/octet-stream';
+
     fs.readFile(filePath, (err, content) => {
         if (err) {
             if (err.code === 'ENOENT') {
@@ -26,6 +39,7 @@ const server = http.createServer((req, res) => {
         }
     });
 });
+
 const port = 3000;
 server.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
