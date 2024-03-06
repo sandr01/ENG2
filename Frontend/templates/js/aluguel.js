bc83a8a92381dd829589cc864fe9f94352b9a5c4
Gardiny <igor20091983@gmail.com>
2024-03-05 23:32:07 -0300

    aluguel

--- Frontend/templates/js/aluguel.js
+++ Frontend/templates/js/aluguel.js
@@ -1,10 +1,6 @@
-// aluguel.js
-
 function adicionarItem() {
-    // Obter os valores do formulário
     var setor = document.getElementById("setor").value;
     var servidor = document.getElementById("servidor").value;
-    var matricula = document.getElementById("matricula").value;
     var dataEmprestimo = document.getElementById("dataEmprestimo").value;
     var dataDevolucao = document.getElementById("dataDevolucao").value;
     var equipamento = document.getElementById("equipamento").value;
@@ -12,55 +8,25 @@ function adicionarItem() {
     var quantidade = document.getElementById("quantidade").value;
     var observacao = document.getElementById("observacao").value;
 
-    // Criar uma nova linha na tabela
     var table = document.getElementById("estoque").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.rows.length);
 
-    // Adicionar células à nova linha
-    var cellSetor = newRow.insertCell(0);
-    var cellServidor = newRow.insertCell(1);
-    var cellMatricula = newRow.insertCell(2);
-    var cellDataEmprestimo = newRow.insertCell(3);
-    var cellDataDevolucao = newRow.insertCell(4);
-    var cellEquipamento = newRow.insertCell(5);
-    var cellDescricaoEquipamento = newRow.insertCell(6);
-    var cellQuantidade = newRow.insertCell(7);
-    var cellObservacao = newRow.insertCell(8);
-    var cellAcoes = newRow.insertCell(9);
+    newRow.insertCell(0).innerHTML = setor;
+    newRow.insertCell(1).innerHTML = servidor;
+    newRow.insertCell(2).innerHTML = dataEmprestimo;
+    newRow.insertCell(3).innerHTML = dataDevolucao;
+    newRow.insertCell(4).innerHTML = equipamento;
+    newRow.insertCell(5).innerHTML = descricaoEquipamento;
+    newRow.insertCell(6).innerHTML = quantidade;
+    newRow.insertCell(7).innerHTML = observacao;
 
-    // Preencher as células com os valores do formulário
-    cellSetor.innerHTML = setor;
-    cellServidor.innerHTML = servidor;
-    cellMatricula.innerHTML = matricula;
-    cellDataEmprestimo.innerHTML = dataEmprestimo;
-    cellDataDevolucao.innerHTML = dataDevolucao;
-    cellEquipamento.innerHTML = equipamento;
-    cellDescricaoEquipamento.innerHTML = descricaoEquipamento;
-    cellQuantidade.innerHTML = quantidade;
-    cellObservacao.innerHTML = observacao;
-
-    // Adicionar um botão de remoção
+    var cellAcoes = newRow.insertCell(8);
     var removeButton = document.createElement("button");
     removeButton.innerHTML = "Remover";
-    removeButton.onclick = function () {
-        // Remover a linha ao clicar no botão
-        table.deleteRow(newRow.rowIndex);
+    removeButton.onclick = function() {
+        table.deleteRow(newRow.rowIndex - 1);
     };
     cellAcoes.appendChild(removeButton);
 
-    // Limpar os campos do formulário
     limparCampos();
-}
-
-// Função para limpar os campos do formulário
-function limparCampos() {
-    document.getElementById("setor").value = "";
-    document.getElementById("servidor").value = "";
-    document.getElementById("matricula").value = "";
-    document.getElementById("dataEmprestimo").value = "";
-    document.getElementById("dataDevolucao").value = "";
-    document.getElementById("equipamento").value = "";
-    document.getElementById("descricaoEquipamento").value = "";
-    document.getElementById("quantidade").value = "";
-    document.getElementById("observacao").value = "";
 }
